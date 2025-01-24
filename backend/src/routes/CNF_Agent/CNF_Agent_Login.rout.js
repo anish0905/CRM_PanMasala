c37b3535516dd9ecf1c0b2810782f2978836f123
samraatc <samraat1550@gmail.com>
2025-01-24 17:27:48 +0530

    controler path updated

--- backend/src/routes/CNF_Agent/CNF_Agent_Login.rout.js
+++ backend/src/routes/CNF_Agent/CNF_Agent_Login.rout.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { loginCNFAgent } = require("../../controllers/CNF_Agent/CNF_Agent.Controller"); 
+const { loginCNFAgent } = require("../../controllers/CNF_Agent/CNF_Agent_Login.controller"); 
 const router = express.Router();
 
 // Route for CNF Agent Login
