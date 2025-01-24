4a5fe6f8a138ccf8853a6c2f02668644097205fe
Ramnarayan Mandal <154451608+RamnarayanMandal@users.noreply.github.com>
2025-01-24 15:18:58 +0530

    added sidebar

--- Administration/src/App.jsx
+++ Administration/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Login from "./pages/Admin/Login";
+import AdminDashbord from "./pages/Admin/AdminDashbord";
 function App() {
   const navigate = useNavigate();
   const email = localStorage.getItem("email");
@@ -11,6 +12,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/AdminDashBoard" element={<AdminDashbord />} />
     </Routes>
   );
 }
