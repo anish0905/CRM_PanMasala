e0e57f9a30b0d9c1243c2e089c7385edfbb6b6f8
Anish kumar <anishanish0102@gmail.com>
2025-01-27 16:01:09 +0530

    dustributerRegister completed

--- backend/src/routes/Distributor/Distributor.Routes.js
+++ backend/src/routes/Distributor/Distributor.Routes.js
@@ -1,27 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {registerUser,loginUser,currentUser, getStateCity,GetAllUser,getUserDetailsByEmail, getDistributorBySuperByID,updateUser,deleteUser}= require("../../controllers/Distributor/Distributor.Controller");
+const {
+  registerUser,
+  loginUser,
+  currentUser,
+  getStateCity,
+  GetAllUser,
+  getUserDetailsByEmail,
+  getDistributorBySuperByID,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/Distributor/Distributor.Controller");
 const executiveValidateToken = require("../../middleware/executiveValidateTokenHandler");
 
-
 //Register
-// router.post("/registerDistributor",registerUser);
+router.post("/register", registerUser);
 
 //Login
 
-router.post("/loginexDistributor",loginUser);
+router.post("/loginexDistributor", loginUser);
 
-router.post("/email",getUserDetailsByEmail)
+router.post("/email", getUserDetailsByEmail);
 
-router.put("/update/:id", updateUser)
+router.put("/update/:id", updateUser);
 
-router.delete("/:id", deleteUser)
+router.delete("/:id", deleteUser);
 
 //Current user information
 
-router.get("/currentDistributor",executiveValidateToken,currentUser);
-router.get("/getAlluser",GetAllUser)
-router.get('/getStateCity/:email', getStateCity);
-router.get('/superStockist/:id', getDistributorBySuperByID);
+router.get("/currentDistributor", executiveValidateToken, currentUser);
+router.get("/getAlluser", GetAllUser);
+router.get("/getStateCity/:email", getStateCity);
+router.get("/superStockist/:id", getDistributorBySuperByID);
 
-module.exports =router;
+module.exports = router;
